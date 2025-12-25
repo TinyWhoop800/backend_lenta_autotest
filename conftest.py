2e3604ab0a7c26a6ad9e6557338a11e254e6508d
Artem Galkin <a.galkin@limehd.tv>
2025-12-25 15:46:14 +0300

    review conftest.py

--- conftest.py
+++ conftest.py
@@ -9,7 +9,6 @@ from assertions.response_validator import check_status
 from models.auth.post_guest_login import PostGuestLogin
 from models.user.delete_user import DeleteUser
 
-
 @pytest.fixture(scope="session")
 def api_client():
     client = APIClient(settings.BASE_URL)
@@ -22,25 +21,50 @@ def app_config(request):
     time.sleep(1)
     return request.param
 
+@pytest.fixture(scope="session")
+def session_tokens(api_client):
+    tokens = {}
+    for app_config in APPS_LENTA:
+        login_response = post_guest_login(api_client, app_config)
+        check_status(login_response, 200)
+        tokens[f"{app_config['app_name']}_{app_config['platform']}"] = login_response.json()["token"]
+
+    yield tokens
+    for app_config in APPS_LENTA:
+        token_key = f"{app_config['app_name']}_{app_config['platform']}"
+        delete_user(api_client, app_config, tokens[token_key])
 
 @pytest.fixture
-def authorized_token(app_config, api_client):
-    """Только авторизация БЕЗ cleanup"""
-    with allure.step("Получаем guest token"):
-        login_response = post_guest_login(api_client, app_config)
-        login_json = login_response.json()
-        check_status(login_response, 200)
-        token = login_json["token"]
-    yield token
+def app_token(app_config, session_tokens):
+    """Токен для текущей app_config"""
+    key = f"{app_config['app_name']}_{app_config['platform']}"
+    return session_tokens[key]
 
-@pytest.fixture
-def guest_token(app_config, api_client):
-    with allure.step("Создаем аккаунт и получаем токен"):
-        login_response = post_guest_login(api_client, app_config)
-        login_json = login_response.json()
-        check_status(login_response, 200)
-        token = login_json["token"]
-    yield token
-    with allure.step("Удаляем аккаунт"):
-        delete_response = delete_user(api_client, app_config, token)
-        check_status(delete_response, 200)
+# @pytest.fixture(params=APPS_LENTA, ids=lambda app: f"{app['app_name']}_{app['platform']}")
+# def app_config(request):
+#     import time
+#     time.sleep(1)
+#     return request.param
+#
+#
+# @pytest.fixture
+# def authorized_token(app_config, api_client):
+#     """Только авторизация БЕЗ cleanup"""
+#     with allure.step("Получаем guest token"):
+#         login_response = post_guest_login(api_client, app_config)
+#         login_json = login_response.json()
+#         check_status(login_response, 200)
+#         token = login_json["token"]
+#     yield token
+#
+# @pytest.fixture
+# def guest_token(app_config, api_client):
+#     with allure.step("Создаем аккаунт и получаем токен"):
+#         login_response = post_guest_login(api_client, app_config)
+#         login_json = login_response.json()
+#         check_status(login_response, 200)
+#         token = login_json["token"]
+#     yield token
+#     with allure.step("Удаляем аккаунт"):
+#         delete_response = delete_user(api_client, app_config, token)
+#         check_status(delete_response, 200)
